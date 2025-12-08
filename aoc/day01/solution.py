adf9a270dc8f6df0edc6acdeed5eb34d9876113d
Rob Denman <rob.denman@datasite.com>
2025-12-07 19:32:23 -0600

    day 1 partial completion

--- aoc/day01/solution.py
+++ aoc/day01/solution.py
@@ -2,11 +2,37 @@ from aoc.utils import read_lines
 
 
 def part1(lines):
-    pass
+    count = 0
+    pos = 50
+    for line in lines:
+        if line.startswith("L"):
+            pos = (pos - int(line[1:])) % 100
+        elif line.startswith("R"):
+            pos = (pos + int(line[1:])) % 100
+        if pos == 0:
+            count += 1
+    return count
 
 
 def part2(lines):
-    pass
+    count = 0
+    pos = 50
+    for line in lines:
+        if line.startswith("L"):
+            pos = pos - int(line[1:])
+        elif line.startswith("R"):
+            pos = pos + int(line[1:])
+        if pos == 0:
+            count += 1
+        elif pos > 99:
+            while pos > 99:
+                pos -= 100
+                count += 1
+        elif pos < 0:
+            while pos < 0:
+                pos += 100
+                count += 1
+    return count
 
 
 if __name__ == "__main__":
